acaa8dd26291478b5535dd0fd0afa39e5e53e915
jeparlefrancais <jeparlefrancais21@gmail.com>
2025-05-13 11:55:03 -0400

    Use luau file extension

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-scripts/build-single-file.sh .darklua-bundle.json build/tag-effect.lua
-scripts/build-single-file.sh .darklua-bundle-dev.json build/debug/tag-effect.lua
+scripts/build-single-file.sh .darklua-bundle.json build/tag-effect.luau
+scripts/build-single-file.sh .darklua-bundle-dev.json build/debug/tag-effect.luau
 scripts/build-roblox-model.sh .darklua.json build/tag-effect.rbxm
 scripts/build-roblox-model.sh .darklua-dev.json build/debug/tag-effect.rbxm
